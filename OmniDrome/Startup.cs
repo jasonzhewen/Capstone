ff30952cf37a806483618c1c52503d917de18c2f
Zhewen Sun <jasonsun0207@gmail.com>
2016-01-14 12:40:39 -0500

    New Login

---
+++ OmniDrome/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OmniDrome.Startup))]
+namespace OmniDrome
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
